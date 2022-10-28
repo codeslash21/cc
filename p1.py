3f56ee4df46debf21509f7096d821f98162abc9c
Soumyadip <somdipguha2456.ind@gmail.com>
2022-10-28 14:48:12 +0530

    python file added.

--- p1.py
+++ p1.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 a=[[0]*1000]*1000
 b=[[0]*1000]*1000
@@ -7,11 +8,11 @@ for i in range(1000):
     for j in range(1000):
         a[i][j]=random.randint(1,100)
         b[i][j]=random.randint(1,100)
-
+start = time.time()
 for i in range(1000):
     print("Row: ", i)
     for j in range(1000):
         for k in range(1000):
             res[i][j]+=a[i][k]*b[k][j]
-
-print("Multiplication completed")
+end = time.time()
+print("Multiplication completed. Time taken {} s".format(end-start))
